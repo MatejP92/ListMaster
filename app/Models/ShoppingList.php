d4c21b382b0fa386ea9aec9d531b1da5192531f3
Matej <matejpal92@gmail.com>
2024-07-11 21:24:05 +0200

    Add user id to shopping list table and create a relationship

--- app/Models/ShoppingList.php
+++ app/Models/ShoppingList.php
@@ -4,14 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ShoppingList extends Model
 {
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'name',
         'category'
     ];
 
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2024_07_11_173659_create_shopping_lists_table.php
+++ database/migrations/2024_07_11_173659_create_shopping_lists_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('shopping_lists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
             $table->string('category')->nullable();
             $table->timestamps();
