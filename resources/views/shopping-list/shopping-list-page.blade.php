c35cdb27066808d8912daf6a717ad38e0857b542
Matej <matejpal92@gmail.com>
2024-07-12 12:43:50 +0200

    Add auth and guest part to shopping list.

--- resources/views/shopping-list/shopping-list-page.blade.php
+++ resources/views/shopping-list/shopping-list-page.blade.php
@@ -5,24 +5,37 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Here is your shopping list") }}
-                </div>
+    @auth
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Here is your shopping list") }}
+                    </div>
 
-                {{-- FORM --}}
-                <div class="pb-6">
-                    <livewire:create-shopping-list />
-                </div>
+                    {{-- FORM --}}
+                    <div class="pb-6">
+                        <livewire:create-shopping-list />
+                    </div>
 
-                {{-- TABLE --}}
-                <div>
+                    {{-- TABLE --}}
+                    <div>
 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endauth
+    @guest
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Please login") }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endguest
 
 </x-app-layout>
