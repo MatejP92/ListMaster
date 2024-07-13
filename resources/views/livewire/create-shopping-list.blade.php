388763f3d3fbe0fbfce5685b341e793420997b09
Matej <matejpal92@gmail.com>
2024-07-13 10:11:47 +0200

    Update css.

--- resources/views/livewire/create-shopping-list.blade.php
+++ resources/views/livewire/create-shopping-list.blade.php
@@ -3,16 +3,16 @@
         @csrf
         <div class="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
             <div class="flex-1">
-                <x-text-input id="item" placeholder="Item" class="w-full ml-2" wire:model="form.item" />
+                <x-text-input id="item" placeholder="Item" class="md:w-full w-2/3 ml-2" wire:model="form.item" />
                 @error('form.item')
                     <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
                 @enderror
             </div>
             <div class="flex-1">
-                <x-text-input id="note" placeholder="Note" class="w-full ml-2" wire:model="form.note" />
+                <x-text-input id="note" placeholder="Note" class="md:w-full w-2/3 ml-2" wire:model="form.note" />
             </div>
             <div>
-                <x-primary-button class="ml-2 md:mx-10 w-full md:w-auto">Add</x-primary-button>
+                <x-primary-button class="md:mx-10 w-1/3 md:w-2/3 md:my-1">Add</x-primary-button>
             </div>
         </div>
     </form>
