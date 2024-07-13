7e44b3f51b820f0d138498de523e15ce03b50a27
Matej <matejpal92@gmail.com>
2024-07-13 10:05:45 +0200

    comment out 1 column from shopping list table.

--- resources/views/livewire/show-shopping-list.blade.php
+++ resources/views/livewire/show-shopping-list.blade.php
@@ -77,9 +77,9 @@
 
                 @empty
                     <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{-- <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             /
-                        </th>
+                        </th> --}}
                         <td class="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white font-bold">
                             /
                         </td>
